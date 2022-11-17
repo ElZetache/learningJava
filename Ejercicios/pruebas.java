4c4dc2ba3766a3631b10309ae1bf6c02aa1a0b3e
ElZetache <96047954+ElZetache@users.noreply.github.com>
2022-11-17 13:11:24 +0100

    Update pruebas.java

--- Ejercicios/pruebas.java
+++ Ejercicios/pruebas.java
@@ -1,51 +1,24 @@
+
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class pruebas {
     static public void main(String[] args) {
-        int[] a = new int[0];
-        int[] b = new int[0];
-        System.out.println(comp(a, b));
+        System.out.println(findUniq(new double[] { 1, 1, 1, 2, 1, 1 }));
     }
 
-    public static boolean comp(int[] a, int[] b) {
-        boolean res = true;
-        boolean encontrado = false;
-        //compruebo que los valores no esten a null, si lo estan retorno false
-        if (a != null && b != null) {
-            if (a.length != b.length)
-                res = false;
-        } else {
-            res = false;
-        }
-        //en caso de que ambos valores tengan datos los recorro
-        for (int val_a : a) {
-            encontrado = false;
+    public static double findUniq(double arr[]) {
+        double res = 0;
+        //double[] arr_ord = Arrays.sort(arr);
+        Arrays.sort(arr); 
 
-            System.out.println(val_a);
-            System.out.println("-------");
-            int long_b = b.length;
-            for (int x = 0; x < long_b; x++) {
-                if (Math.pow(val_a, 2) == b[x] && encontrado == false) {
-                    //si encuentra el valor lo marco como encontrado e informo el elemento con -1 para
-                    //que no lo vuelva a coger
-                    encontrado = true;
-                    b[x] = -1;
-
-                }
-                System.out.println(b[x]);
-
-            }
-            //si hay algun valor que no encuentra devuelvo false
-            if (encontrado != true) {
-                res = false;
-            }
-        }
-        //si ambos campos estan vacios lo marco como true
-        if (a != null && b != null) {
-            if (a.length == 0 && b.length == 0)
-                res = true;
-        }
+        for(int x = 0; x<arr.length-1; x++){
+            if(x == 0) res = arr[x]; //take the first number
+            if(arr[x] == res){
+                res = arr[x+1]; //if actual number equals saved number take next
+            } 
+        }  
         return res;
-    }
-
+      }
 }
