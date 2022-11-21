696ea4c0fd6dead943328210f67e1fbf7b9d15d5
ElZetache <zetachewtf@gmail.com>
2022-11-21 20:23:31 +0100

    Update excepciones.java

--- teoria/excepciones.java
+++ teoria/excepciones.java
@@ -1,17 +1,17 @@
 package teoria;
-
-import java.util.ArrayList;
+//! lo que esta comentado se puede descomentar para probar las excepciones, esta comentado para que el IDE no me este avisando todo el rato
+//import java.util.ArrayList;
 
 public class excepciones {
     public static void main(String[] args) throws ArithmeticException {
         int x = 3;
         int[] y = new int[1];
-        ArrayList<String> z = null;
+        //ArrayList<String> z = null;
 
         try {
             x = x / 0;
             y[2] = 3;
-            z.get(3);
+         //   z.get(3);
         } catch (ArithmeticException e) {
             System.out.println("no se puede dividir entre 0");
         } catch (ArrayIndexOutOfBoundsException e) {
